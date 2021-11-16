0a5dcc51d25841ab4f538059a3c51b28b99b71da
Ashwani Yadav <ashyadavash@gmail.com>
2021-11-17 02:11:46 +0530

    added files

---
+++ data/authors/default.md
@@ -0,0 +1,14 @@
+---
+name: Ashwani Yadav
+avatar: /static/images/avatar.png
+occupation: UG at the IIT Patna
+company: Zomato
+email: ashyadavash@gmail.com
+twitter: https://twitter.com/ashwani_iitp
+linkedin: https://www.linkedin.com/in/ashwaniydv
+github: https://github.com
+---
+
+Ashwani Yadav is a student of Electrical Engineering at the Indian Institute of Technology Patna. 
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.
---
+++ pages/_app.js
@@ -0,0 +1,33 @@
+import '@/css/tailwind.css'
+import '@/css/prism.css'
+import 'nprogress/nprogress.css'
+import nprogress from 'nprogress/nprogress.js'
+
+import { ThemeProvider } from 'next-themes'
+import Head from 'next/head'
+import Router from 'next/router'
+
+import Analytics from '@/components/analytics'
+import LayoutWrapper from '@/components/LayoutWrapper'
+import { ClientReload } from '@/components/ClientReload'
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+
+Router.events.on('routeChangeStart', () => nprogress.start())
+Router.events.on('routeChangeComplete', () => nprogress.done())
+Router.events.on('routeChangeError', () => nprogress.done())
+
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider attribute="class">
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      {isDevelopment && <ClientReload />}
+      <Analytics />
+      <LayoutWrapper>
+        <Component {...pageProps} />
+      </LayoutWrapper>
+    </ThemeProvider>
+  )
+}
